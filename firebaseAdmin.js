c2a1f008db0225f96684771397dca6ff06998273
Shikhar Tandon <shikhartandon026@gmail.com>
2024-08-05 14:47:52 +0530

    some commit

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json'); 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
- databaseURL: 'test.firebaseio.com'
+ databaseURL: 'https://console.firebase.google.com/project/intern-area-1b584/database/intern-area-1b584-default-rtdb/data/~2F'
 });
 
 module.exports = { admin };
