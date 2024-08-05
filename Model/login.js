c841dd2cbbff2c36690b22a071eadf3e455ea431
Shikhar Tandon <shikhartandon026@gmail.com>
2024-08-05 15:45:16 +0530

    some commit

--- Model/login.js
+++ Model/login.js
@@ -3,10 +3,7 @@ const LoginShcema=new mongoose.Schema({
     browser: String,
     os: String,
     device: String,
+    ip: Number,
     
-    createAt:{
-        type:Date,
-        default:Date.now,
-    },
 })
 module.exports=mongoose.model("Login",LoginShcema)
